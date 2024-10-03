9f18c2af02f532719b576f1ccd3b5bcab625db0c
mharpole <mharpole@deloitte.com>
2024-10-03 12:52:14 -0400

    auth test

--- func.py
+++ func.py
@@ -1,21 +1,16 @@
+import oci
 import io
-import json
-import logging
-
-from fdk import response
-
 
 def handler(ctx, data: io.BytesIO = None):
-    name = "World"
-    try:
-        body = json.loads(data.getvalue())
-        name = body.get("name")
-    except (Exception, ValueError) as ex:
-        logging.getLogger().info('error parsing json payload: ' + str(ex))
-
-    logging.getLogger().info("Inside Python Hello World function")
-    return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
-        headers={"Content-Type": "application/json"}
-    )
+    signer = oci.auth.signers.get_resource_principals_signer()
+    
+    # Example: List objects in an OCI Object Storage bucket
+    object_storage_client = oci.object_storage.ObjectStorageClient(config={}, signer=signer)
+    namespace = object_storage_client.get_namespace().data
+    bucket_name = "your_bucket_name"
+    
+    objects = object_storage_client.list_objects(namespace, bucket_name)
+    for obj in objects.data.objects:
+        print(obj.name)
+    
+    return "Function executed successfully"
